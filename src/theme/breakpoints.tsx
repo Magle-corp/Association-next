390870b7c2f2cd21070824fc94b6c14fe6f9d341
Magle-corp <wolff.kevin@protonmail.com>
2021-11-14 22:51:12 +0100

    Fix theme breakpoint + fix theme typo

--- src/theme/breakpoints.tsx
+++ src/theme/breakpoints.tsx
@@ -1,7 +1,7 @@
 const breakpoints: object = {
-  mobile: 768,
-  desktop: 1024,
-  maximumWidth: 1280,
+  mobile: '768px',
+  desktop: '1024px',
+  maximumWidth: '1280px',
 };
 
 export default breakpoints;
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,17 +1,46 @@
 import { createGlobalStyle } from 'styled-components';
 import colors from './colors';
 import breakpoints from './breakpoints';
+import typography from './typography';
+
+import FjallaOneWoff from './font/FjallaOneWoff.woff';
+import FjallaOneWoff2 from './font/FjallaOneWoff2.woff2';
+import RobotoWoff from './font/RobotoWoff.woff';
+import RobotoWoff2 from './font/RobotoWoff2.woff2';
 
 const theme: object = {
   colors,
   breakpoints,
+  typography,
 };
 
 export { theme };
 
 export const GlobalStyle = createGlobalStyle`
-* {
-  padding: 0;
-  margin: 0;
-}
+  html {
+    font-size: 62.5%;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  @font-face {
+    font-family: 'FjallaOne';
+    src: local('FjallaOne'), local('FjallaOne'),
+    url(${FjallaOneWoff}) format('woff'),
+    url(${FjallaOneWoff2}) format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'Roboto';
+    src: local('Roboto'), local('Roboto'),
+    url(${RobotoWoff}) format('woff'),
+    url(${RobotoWoff2}) format('woff2');
+    font-weight: 300;
+    font-style: normal;
+  }
 `;
