b11e9ca285f3a1ce43c384f34a8f86592c51da24
Magle-corp <wolff.kevin@protonmail.com>
2021-11-14 22:50:39 +0100

    Add theme typography

---
+++ src/theme/typography.tsx
@@ -0,0 +1,60 @@
+const titleFontFamily = `font-family: FjallaOne, Arial, sans-serif;`;
+const fontFamily = `font-family: Roboto, Arial, sans-serif;`;
+
+const variant: object = {
+  h1: `
+    ${titleFontFamily}
+    font-size: 4rem;
+    line-height: 4.8rem;
+    font-weight: 900;
+    text-decoration: none;
+    `,
+  h2: `
+    ${titleFontFamily}
+    font-size: 3.2rem;
+    line-height: 4rem;
+    font-weight: 800;
+    text-decoration: none;
+`,
+  h3: `
+    ${titleFontFamily}
+    font-size: 2.4rem;
+    line-height: 3.2rem;
+    font-weight: 700;
+    text-decoration: none;
+`,
+  h4: `
+    ${titleFontFamily}
+    font-size: 2rem;
+    line-height: 2.8rem;
+    font-weight: 600;
+    text-decoration: none;
+`,
+  p: `
+    ${fontFamily}
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    font-weight: 400;
+    text-decoration: none;
+`,
+  button: `
+    ${fontFamily}
+    padding: 5px 7px;
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    font-weight: 700;
+    text-decoration: none;
+    background-color: #ffffff;
+    border: 2px solid #000000;
+    border-radius: 3px;
+    cursor: pointer;
+`,
+  inline_link: `
+    ${fontFamily}
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    font-weight: 400;
+`,
+};
+
+export default variant;
