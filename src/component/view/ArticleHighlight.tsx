272b8d668087191c4a42140ee4ebbd4baab236e6
Magle-corp <wolff.kevin@protonmail.com>
2021-12-03 21:07:14 +0100

    Fix component ArticleHighlight image size

--- src/component/view/ArticleHighlight.tsx
+++ src/component/view/ArticleHighlight.tsx
@@ -49,7 +49,7 @@ const ArticleHighlight = ({ article }: Props) => {
     <ArticleWrapper>
       <ImageWrapper>
         <Image
-          src={`${process.env.BASE_URL}${article.background.formats.small.url}`}
+          src={`${process.env.BASE_URL}${article.background.formats.large.url}`}
           layout="fill"
           objectFit="cover"
           alt={article.background.alternativeText}
