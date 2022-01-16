de95fe869b769cff4551d9eadc152015c36f2177
Magle-corp <wolff.kevin@protonmail.com>
2022-01-16 19:10:25 +0100

    Remove AppContext

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { AppWrapper } from '../src/AppContext';
 import { theme, GlobalStyle } from '../src/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <AppWrapper>
-        <Component {...pageProps} />
-      </AppWrapper>
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,48 @@
 // Use.
-import { useAppContext } from '../src/AppContext';
+import { Homepage, Identity } from '../src/type';
 import { Header, HeroBanner, Footer } from '../src/component';
 import { Layout, Main } from '../src/ui';
-import { Homepage, Identity } from '../src/type';
 
 interface Props {
-  homepage?: Homepage;
-  identity?: Identity;
+  homepage: Homepage;
+  identity: Identity;
 }
 
 /**
  * Provide home page.
+ *
+ * @param homepage
+ *   Strapi custom content type "Page d'accueil".
+ * @param identity
+ *   Strapi custom content type "Identite".
  */
-const Home = () => {
-  console.log(useAppContext());
-  const { homepage, identity } = useAppContext() as Props;
-
+const Home = ({ homepage, identity }: Props) => {
   return (
     <>
-      {homepage && identity && (
-        <>
-          <Header identity={identity} />
-          <Layout variant="mono">
-            <Main variant="mono">
-              <HeroBanner homepage={homepage} />
-            </Main>
-          </Layout>
-          <Footer identity={identity} />
-        </>
-      )}
+      <Header identity={identity} />
+      <Layout variant="mono">
+        <Main variant="mono">
+          <HeroBanner homepage={homepage} />
+        </Main>
+      </Layout>
+      <Footer identity={identity} />
     </>
   );
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const HpQuery = `/page-d-accueil`;
+  const HpResult = await fetch(`${process.env.BASE_URL}${HpQuery}`);
+  const homepage = await HpResult.json();
+
+  const identityQuery = `/identite`;
+  const identityResult = await fetch(`${process.env.BASE_URL}${identityQuery}`);
+  const identity = await identityResult.json();
+
+  return {
+    props: { homepage, identity },
+    revalidate: 60 * 60,
+  };
+}
